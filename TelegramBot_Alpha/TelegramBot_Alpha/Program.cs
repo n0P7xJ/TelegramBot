2e65ba98507a2ec0e1eb21d0b65c0741241bb821
n0P7xJ <124531921+n0P7xJ@users.noreply.github.com>
2025-02-06 19:16:14 +0200

    First code (Create Telegram BOT, include framework)

---
+++ TelegramBot_Alpha/TelegramBot_Alpha/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Telegram.Bot;
+
+var bot = new TelegramBotClient("8071381491:AAEf6RXLsFNcNlzLiSXT4XtnE9I-oqD_vVc");
+var me = await bot.GetMe();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
